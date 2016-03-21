0e14dca2490bbf678ac4424bf679c7d9a413d2a8
Paul Batchelor <pbatch@ccrma.stanford.edu>
2016-03-21 11:17:00 -0400

    finished part 2. 'clown shading'

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 
-const int width = 200;
-const int height = 200;
+const int width = 800;
+const int height = 800;
 
 void swap(int *a, int *b)
 {
@@ -69,38 +69,47 @@ Vec3f barycentric(Vec2i *pts, Vec2i P) {
     return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 }
 
-void triangle(Vec2i *pts, TGAImage *image, TGAColor color)
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage *image, TGAColor color)
 {
-    Vec2i bboxmin(image->get_width() - 1, image->get_height() - 1);
-    Vec2i bboxmax(0, 0);
-    Vec2i clamp(image->get_width() - 1, image->get_height() - 1);
+    if(t0.y == t1.y && t0.y == t2.y) return;
 
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 2; j++) {
-            bboxmin[j] = max(0, min(bboxmin[j], pts[i][j]));
-            bboxmax[j] = min(clamp[j], max(bboxmin[j], pts[i][j]));
+    if(t0.y > t1.y) swap_vec2(&t0, &t1);
+    if(t0.y > t2.y) swap_vec2(&t0, &t2);
+    if(t1.y > t2.y) swap_vec2(&t1, &t2);
+
+    int total_height = t2.y - t0.y; 
+
+    for(int i = 0; i < total_height; i++) {
+        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+        float alpha = (float)i / total_height;
+        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+        if(A.x > B.x) swap_vec2(&A, &B);
+        for(int j = A.x; j<=B.x; j++) {
+            image->set(j, t0.y + i, color);
         }
     }
-
-    Vec2i P;
-
-    for(P.x = bboxmin.x; P.x<=bboxmax.x; P.x++) {
-        for(P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
-            Vec3f bc_screen = barycentric(pts, P);
-            if(bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-            image->set(P.x, P.y, color);
-        }
-    }
-
 }
 
 int main(int argc, char** argv) {
+
+    Model *model = new Model("obj/african_head.obj");
     TGAImage frame(width, height, TGAImage::RGB);
-   
-    Vec2i pts[3] = {Vec2i(10, 10), Vec2i(100, 30), Vec2i(190, 160)}; 
+  
+    for(int i=0; i< model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        for(int j=0; j < 3; j++) {
+            Vec3f world_coords = model->vert(face[j]);
+            screen_coords[j] = Vec2i((world_coords.x + 1.) * width / 2., 
+                (world_coords.y + 1.) * height / 2.); 
+        }
+        triangle(screen_coords[0], screen_coords[1], screen_coords[2], 
+            &frame, TGAColor(rand() % 255, rand() % 255, rand() % 255));
 
-    triangle(pts, &frame, TGAColor(255, 0, 0));
-
+    }
     frame.flip_vertically();
     frame.write_tga_file("out.tga");
     return 0;
